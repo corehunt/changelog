1839abf9c92c8ea6fe94b6b0cb3d9dc6734f7e0c
coreyroach <corehuntswe@gmail.com>
2025-12-14 17:55:41 -0500

    repo methods to count by

--- api/src/main/java/com/changelog/tickets/repository/EntryRepository.java
+++ api/src/main/java/com/changelog/tickets/repository/EntryRepository.java
@@ -3,5 +3,9 @@ package com.changelog.tickets.repository;
 import com.changelog.tickets.model.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.OffsetDateTime;
+
 public interface EntryRepository extends JpaRepository<Entry, Long> {
+
+    long countByDateAfter(OffsetDateTime date);
 }
--- api/src/main/java/com/changelog/tickets/repository/TicketRepository.java
+++ api/src/main/java/com/changelog/tickets/repository/TicketRepository.java
@@ -1,12 +1,15 @@
 package com.changelog.tickets.repository;
 
 import com.changelog.tickets.model.Ticket;
+import com.changelog.tickets.model.TicketStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-    Page<Ticket> findByStatus(String status, Pageable pageable);
+    Page<Ticket> findByStatus(TicketStatus status, Pageable pageable);
+
+    long countByStatus(TicketStatus status);
 
 }
